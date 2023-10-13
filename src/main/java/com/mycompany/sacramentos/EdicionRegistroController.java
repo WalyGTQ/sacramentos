ee1538ee05cab09716184a76987cac1278220337
walyn <walyn@HPCorei5>
2023-10-13 10:05:26 -0600

    La funcionalidad de Actualizar un registro de bautismo esta funcional

--- src/main/java/com/mycompany/sacramentos/EdicionRegistroController.java
+++ src/main/java/com/mycompany/sacramentos/EdicionRegistroController.java
@@ -7,6 +7,7 @@ package com.mycompany.sacramentos;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -43,6 +44,7 @@ public class EdicionRegistroController implements Initializable {
 
     //Para validar el Check
     private String inscrito;
+    private Integer idBautismo;
 
     /**
      * Initializes the controller class.
@@ -159,10 +161,127 @@ public class EdicionRegistroController implements Initializable {
     //Funcionalidad para actualizar un registro en especifico
     @FXML
     public void _actualizarB() throws IOException, SQLException {
+        PreparedStatement pstmt = null;
+        PreparedStatement pstmt1 = null;
+        PreparedStatement pstmt2 = null;
         if (comparacionB()) {
             // Actualiza la base de datos
+
+            // Crear ventana de diálogo de confirmación
+            if (showConfirmationDialog("Confirmar Actualizacion", "Actualizar registro", "¿Estás seguro? Deseas Actualizar el registro de: " + datosFeligres.getNombre())) {
+                // El usuario ha confirmado la eliminación, Código para eliminar el registro
+
+                Connection connection = ConexionDB.getConexion();
+                try {
+
+                    connection.setAutoCommit(false); // Start transaction
+                    // 1. Obtener el idBautismo basado en la partida y el nombre
+                    String queryId = "SELECT b.idBautismo, f.idFeligres "
+                            + "FROM feligres f "
+                            + "JOIN bautismo b ON f.idFeligres = b.idFeligres "
+                            + "JOIN registrolibro r ON b.idBautismo = r.bautismo_idBautismo "
+                            + "WHERE f.nombre = ? AND r.partida = ?";
+                    PreparedStatement stmtId = connection.prepareStatement(queryId);
+                    stmtId.setString(1, datosFeligres.getNombre());
+                    stmtId.setInt(2, datosFeligres.getPartida());
+                    ResultSet rs = stmtId.executeQuery();
+
+                    if (rs.next()) {
+                        idBautismo = rs.getInt("idBautismo");
+                        int idFeligres = rs.getInt("idFeligres");
+
+                        // Obtén la fecha de nacimiento del DatePicker
+                        LocalDate fechaNacimientoC = dpNacimientoB.getValue();
+
+                        // Calcula la edad
+                        LocalDate hoy = LocalDate.now();
+                        int edad = hoy.getYear() - fechaNacimientoC.getYear();
+                        if (fechaNacimientoC.getDayOfYear() > hoy.getDayOfYear()) {
+                            edad--; // Ajusta la edad si el cumpleaños de este año aún no ha llegado.
+                        }
+                        
+                                //Manejo del ChecBox Modificado :.(
+        boolean check = cbInscritoB.isSelected();
+        if (check) {
+            inscrito = "Si";
+        } else {
+            inscrito = "No";
+        }
+
+
+
+                        // Update Feligres
+                        String sqlFeligres = "UPDATE feligres SET nombre = ?, apellido = ?, nacimiento = ?, edadFeligres = ?, lugarNacimiento = ?, padreFeligres = ?, madreFeligres = ? WHERE idFeligres = ?";
+
+                        pstmt = connection.prepareStatement(sqlFeligres);
+                        pstmt.setString(1, txtNombreB.getText());
+                        pstmt.setString(2, txtApellidoB.getText());
+                        pstmt.setDate(3, Date.valueOf(dpNacimientoB.getValue()));
+                        pstmt.setInt(4, edad);
+                        pstmt.setString(5, txtLugarNacimientoB.getText());
+                        pstmt.setString(6, txtPadreB.getText());
+                        pstmt.setString(7, txtMadreB.getText());
+                        pstmt.setInt(8, idFeligres);
+                        pstmt.executeUpdate();
+
+                        // Update Bautismo
+                        String sqlUpdateBautismo = "UPDATE bautismo SET fechaSacramento = ?, lugarSacramento = ?, padrino = ?, madrina = ? WHERE idFeligres = ?";
+                        pstmt1 = connection.prepareStatement(sqlUpdateBautismo);
+                        pstmt1.setDate(1, Date.valueOf(dpFechaB.getValue()));
+                        pstmt1.setString(2, txtLugarNacimientoB.getText());
+                        pstmt1.setString(3, txtPadrinoB.getText());
+                        pstmt1.setString(4, txtMadrinaB.getText());
+                        pstmt1.setInt(5, idFeligres);
+                        pstmt1.executeUpdate();
+
+                        // Update RegistroLibro
+                        String sqlUpdateRegistroLibro = "UPDATE registroLibro SET libro = ?, folio = ?, partida = ?, inscritoLibro = ? WHERE bautismo_idBautismo = ?";
+                        pstmt2 = connection.prepareStatement(sqlUpdateRegistroLibro);
+                        pstmt2.setInt(1, Integer.parseInt(txtLibroB.getText()));
+                        pstmt2.setInt(2, Integer.parseInt(txtFolioB.getText()));
+                        pstmt2.setInt(3, Integer.parseInt(txtPartidaB.getText()));
+                        pstmt2.setString(4, inscrito);
+                        pstmt2.setInt(5, idBautismo);
+                        pstmt2.executeUpdate();
+                        
+                        // Update RegistroLibro
+                        String sqlUpdateObservaciones = "UPDATE observacion SET observacion = ? WHERE bautismo_idBautismo = ?";
+                        pstmt2 = connection.prepareStatement(sqlUpdateObservaciones);
+                        pstmt2.setString(1, taObservacionesB.getText());
+                        pstmt2.setInt(2, idBautismo);
+                        pstmt2.executeUpdate();
+
+                        // Registro no encontrado
+                        showAlert("Información", "El registro fue Actualizado Satisfactoriamente.", "Se Actualizo: " + datosFeligres.getNombre(), Alert.AlertType.INFORMATION);
+                        App.setRoot("bautismosVista");
+                        connection.commit(); // Commit transaction
+                    } else {
+                        // Registro no encontrado
+                        showAlert("Información", "No se encontró un registro con ese nombre y partida.", "", Alert.AlertType.INFORMATION);
+                    }
+
+                } catch (SQLException ex) {
+                    connection.rollback();
+                    ex.printStackTrace();
+                    showAlert("Error", "Hubo un error al actualizar el registro.", "", Alert.AlertType.ERROR);
+                } finally {
+                    try {
+                        if (connection != null) {
+                            connection.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } else {
+                // El usuario ha cancelado la eliminación, Cualquier acción que consideres necesario
+                // Registro no encontrado
+                showAlert("Información", "El Usuario a Cancelado la Operacion.", "Ninguna Modificaion Realizada", Alert.AlertType.INFORMATION);
+            }
+
             System.out.println("Cambios detectados");
-                
+
         } else {
             // No hagas nada, ya que no hubo cambios
             System.out.println("Ningun cambio xD");
