f0010db6bf0ae2f20ba14a044d4439a4e50de6a7
walyn <walyn@HPCorei5>
2024-07-25 20:33:39 -0600

    Optimizacion de codigo

--- src/main/java/com/mycompany/sacramentos/ConexionDB.java
+++ src/main/java/com/mycompany/sacramentos/ConexionDB.java
@@ -13,10 +13,9 @@ public class ConexionDB {
 
     private LocalDateTime horaInicioSesion;
     private LocalDateTime horaFinSesion;
-
-    private String UsuarioSesion;
-    private String UsuarioActividad;
-    private int idUsuarioSesion;
+    private final String UsuarioSesion;
+    private final String UsuarioActividad;
+    private final int idUsuarioSesion;
     private String estadoSesion;
     private Connection connection;
 
@@ -24,12 +23,14 @@ public class ConexionDB {
     private static final String USER = "root";
     private static final String PASS = "Admin_123##";
 
+    // Constructor para inicializar los parámetros de sesión
     public ConexionDB(String UsuarioSesion, int idUsuarioSesion, String UsuarioActividad) {
         this.UsuarioSesion = UsuarioSesion;
         this.idUsuarioSesion = idUsuarioSesion;
         this.UsuarioActividad = UsuarioActividad;
     }
 
+    // Método para iniciar la sesión y registrar en la base de datos
     public void iniciarSesion() throws SQLException {
         this.horaInicioSesion = LocalDateTime.now();
         this.estadoSesion = "activa";
@@ -43,14 +44,14 @@ public class ConexionDB {
             preparedStatement.setString(4, estadoSesion);
             preparedStatement.setString(5, UsuarioActividad);
             preparedStatement.executeUpdate();
+
             // Obtener las claves generadas
             try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     long generatedId = generatedKeys.getLong(1);
-                    Integer Id = (int) generatedId;
-                    System.out.println("ID generado: " + Id);
-                    SingletonDatosUsuario.getInstance().getDatosUsuario().setIdentidad(Id);
-                    // Puedes almacenar el ID generado si lo necesitas en la sesión u otra variable
+                    int id = (int) generatedId;
+                    System.out.println("ID generado: " + id);
+                    SingletonDatosUsuario.getInstance().getDatosUsuario().setIdentidad(id);
                 } else {
                     System.out.println("No se generó ningún ID.");
                 }
@@ -60,14 +61,17 @@ public class ConexionDB {
         System.out.println("Sesión iniciada para el usuario: " + UsuarioSesion);
     }
 
+    // Método para cerrar la sesión y actualizar el registro en la base de datos
     public void cerrarSesion() throws SQLException {
         this.horaFinSesion = LocalDateTime.now();
         this.estadoSesion = "cerrada";
         this.connection = DriverManager.getConnection(URL, USER, PASS);
-        Duration duracion = Duration.between(SingletonDatosUsuario.getInstance().getDatosUsuario().getFechaHoraRegistro(), horaFinSesion);
-        // Convertir la duración a minutos
-        long duracionEnMinutos = duracion.toSeconds();
 
+        // Calcular la duración de la sesión
+        Duration duracion = Duration.between(SingletonDatosUsuario.getInstance().getDatosUsuario().getFechaHoraRegistro(), horaFinSesion);
+        long duracionEnSegundos = duracion.toSeconds();
+
+        // Formatear la fecha y hora de inicio de sesión
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = SingletonDatosUsuario.getInstance().getDatosUsuario().getFechaHoraRegistro().format(formatter);
 
@@ -75,23 +79,25 @@ public class ConexionDB {
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, estadoSesion);
             preparedStatement.setObject(2, horaFinSesion);
-            preparedStatement.setLong(3, duracionEnMinutos); // Asumiendo que duracionEnMinutos es de tipo long
+            preparedStatement.setLong(3, duracionEnSegundos);
             preparedStatement.setString(4, UsuarioSesion);
             preparedStatement.setInt(5, idUsuarioSesion);
-            preparedStatement.setObject(6, SingletonDatosUsuario.getInstance().getDatosUsuario().getIdentidad()); // Asegúrate de que el tipo sea compatible
+            preparedStatement.setObject(6, SingletonDatosUsuario.getInstance().getDatosUsuario().getIdentidad());
 
             preparedStatement.executeUpdate();
             System.out.println(SingletonDatosUsuario.getInstance().getDatosUsuario().getFechaHoraRegistro());
             System.out.println(formattedDateTime);
         }
 
-       // if (connection != null && !connection.isClosed()) {
+        // Cerrar la conexión si está abierta
+        if (connection != null && !connection.isClosed()) {
             connection.close();
-       // }
+        }
 
         System.out.println("Sesión cerrada para el usuario: " + UsuarioSesion);
     }
 
+    // Método estático para obtener la conexión a la base de datos
     public static Connection getConexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
