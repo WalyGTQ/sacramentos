b9205aae18573eae088401205d58dc8c4f4c71fc
walyn <walyn@HPCorei5>
2024-07-27 18:58:09 -0600

    realineacion de codigo y estetica prueba de boton en vista de informes

--- src/main/java/com/mycompany/sacramentos/BautismosVistaController.java
+++ src/main/java/com/mycompany/sacramentos/BautismosVistaController.java
@@ -15,7 +15,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
